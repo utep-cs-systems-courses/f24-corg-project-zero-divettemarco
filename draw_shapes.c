fb4fe77cc57c0d6b737a0eef633ceef6342f856a
divettemarco <divettemarco@outlook.com>
2024-09-17 19:08:03 -0600

    update with arrow command and font

--- draw_shapes.c
+++ draw_shapes.c
@@ -1,3 +1,4 @@
+// Divette Marco | 2024-09-17
 #include <stdio.h>
 #include "draw.h"
 
@@ -26,3 +27,41 @@ void print_triangle(int leftCol, int size)
   }
 }
 
+// Prints an arrow of specified height whose left col is at leftCol.
+void print_arrow(int leftCol, int size)
+{
+  int endCol = leftCol + size;
+  int pointCol = endCol-3;
+  for (int row = 0; row <= 4; row++) {
+    int col;
+    switch (row){
+    // to create the top and bottom tips of the arrow point.
+    case 0: case 4:
+      for (col = 0; col < endCol; col++){
+	if (col == pointCol){
+	    putchar('*');    
+	}
+	else{
+	  putchar(' ');
+	}
+      }
+      putchar('\n');
+      break;
+    // to continue to create the top and bottom tips of the arrow point.
+    case 1: case 3:
+      for (col = 0; col < pointCol; col++) putchar(' ');
+      for (       ; col < endCol-1;  col++) putchar('*');
+      putchar(' ');
+      putchar('\n');
+      break;
+    // to create the arrow line and create arrow point.
+    case 2:
+      for (col = 0; col < leftCol; col++) putchar(' ');
+      for (       ; col < endCol;  col++) putchar('*');
+      putchar('\n');
+      break;
+    default:      
+      printf("unrecognized option '%row', please try again!", row);
+    }
+  }
+}
